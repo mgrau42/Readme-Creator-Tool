8baabcc5e0eae9b9552ded61ea34d04cdf2023de
Manuel Grau <mgrau@c1r16s4.42madrid.com>
2023-02-22 11:27:24 +0100

    slight modification

--- AICaller.py
+++ AICaller.py
@@ -3,7 +3,7 @@ import openai
 from time import time,sleep
 
 #ADD OPENAI KEY HERE
-openai.api_key = "YOUR_OPENAI_API_KEY"
+openai.api_key = "YOUR_KEY_HERE"
 
 # You can get one in https://platform.openai.com/account/api-keys
 # as of 19/02/2023 Open AI offers a free Trial for experimenting with $18 in free credit that can be used during your first 3 months.
@@ -19,7 +19,7 @@ def save_file(content, filepath):
 	with open(filepath, 'w', encoding='utf-8') as outfile:
 		outfile.write(content)
 
-def gpt3_completion(prompt, model="text-davinci-003", temp=0.1, max_tok=400, top_p=1.0, freq_pen=0.0, pres_pen=0.0, stop=['<<END>>']):
+def gpt3_completion(prompt,temp, model="text-davinci-003", max_tok=400, top_p=1.0, freq_pen=0.0, pres_pen=0.0, stop=['<<END>>']):
 	max_retry = 5
 	retry = 0
 	print('Comunicating with OpenAi...')
